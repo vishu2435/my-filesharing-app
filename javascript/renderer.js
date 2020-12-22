b8d60b94af0dab2f1dd46d33f4d52b14fb889f28
vishu2435 <57234704+vishu2435@users.noreply.github.com>
2020-12-23 03:14:51 +0530

    testing peer connection

--- javascript/renderer.js
+++ javascript/renderer.js
@@ -2,17 +2,73 @@ const fs = require('fs')
 const { ipcRenderer } = require('electron')
 const { dir, log } = require('console')
 const pathConstant = require('path')
-
+ 
 const root = fs.readdirSync(process.env.HOME)
 
 
 class GlobalVariables{
     static filesDocument = document.getElementById('files')
     // static fileObjects = []
+    static SENDER = false
+    static RECEIVER = false
     static backIconContainer = document.getElementById('backIcon')
     static CURRENT_PATH = process.env.HOME
     static pathInfoContainer = document.getElementById('pathInfo')
+    static PEER = new Peer()
+    static CONNECT = null;
+    static connectionBlock = document.getElementById('connection-block')
+    static initialize() {
+        
+        document.getElementById('send-button')
+        .addEventListener('click',()=>{
+            connect(document.getElementById('id-input').value)
+        })
+       
+        
+        document.getElementById('receive-button').addEventListener('click',recieve,{once:true})
+    }
+}
 
+function connect(id){
+    if(!id){
+        alert("Please enter id")
+        return
+    }
+    if(!GlobalVariables.RECEIVER){
+        GlobalVariables.SENDER = true
+        console.log(id)
+        GlobalVariables.PEER.connect(id)
+        let h3 = document.createElement('h3')
+        h3.innerHTML = 'Connecting ... '
+        GlobalVariables.connectionBlock.appendChild(h3)
+        GlobalVariables.PEER.on('error',err=>{
+            GlobalVariables.SENDER=false
+            console.log(`Error in connecting ${err}ds`)
+            h3.innerHTML = `Error in connecting ${err}`
+            setTimeout(()=>{
+                   GlobalVariables.connectionBlock.removeChild(GlobalVariables.connectionBlock.lastElementChild)
+            },2000)
+         
+        })      
+    }else{
+        alert("You are a reciever")
+    }
+ 
+}
+function recieve() {
+    if(!GlobalVariables.SENDER){
+        GlobalVariables.RECEIVER = true
+    console.log(`id is ${GlobalVariables.PEER.id}`);
+    let h2 = document.createElement('h2')
+    h2.innerText=`ID: ${GlobalVariables.PEER.id}`
+    GlobalVariables.connectionBlock.appendChild(h2)
+    GlobalVariables.PEER.on('open',id=>{
+    
+    })
+    }else{
+        alert('You are a sender')
+    }
+    
 }
 
 class Directory{
@@ -66,5 +122,7 @@ function loadDirectories(directoryArray,path){
 }
 window.onload=()=>{
     console.log("onload called")
+    GlobalVariables.initialize()
     loadDirectories(root,process.env.HOME)
+
 }
